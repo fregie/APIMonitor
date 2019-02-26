1e9adc61f3133c0e45f7fee7bebe524d326577bf
fregie <xiaohao950830@live.com>
2019-02-26 11:42:33 +0800

    support mail notify

--- apiTest.go
+++ apiTest.go
@@ -7,15 +7,16 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/smtp"
 	"os"
+	"strings"
 	"time"
 
 	ypclnt "github.com/yunpian/yunpian-go-sdk/sdk"
 )
 
-var APIKey string
-var phones []string
-var smsPrefix string
+var YP YunPianInfo
+var mailInfo MailInfo
 
 const configFile = "./config.json"
 
@@ -28,18 +29,31 @@ type testItem struct {
 	Interval       string            `json:"interval"`
 }
 
+type YunPianInfo struct {
+	Enable    bool     `json:"enable"`
+	APIKey    string   `json:"apiKey"`
+	Phones    []string `json:"phones"`
+	SmsPrefix string   `json:"smsPrefix"`
+}
+
+type MailInfo struct {
+	Enable   bool     `json:"enable"`
+	Username string   `json:"username"`
+	Password string   `json:"password"`
+	Smtp     string   `json:"smtp"`
+	To       []string `json:"to"`
+}
+
 type config struct {
-	APIKey    string     `json:"apiKey"`
-	Phones    []string   `json:"phones"`
-	SmsPrefix string     `json:"smsPrefix"`
-	TestItem  []testItem `json:"testItem"`
+	MailInfo MailInfo    `json:"mail"`
+	YPInfo   YunPianInfo `json:"YunPian"`
+	TestItem []testItem  `json:"testItem"`
 }
 
 func main() {
 	config := loadConfig(configFile)
-	APIKey = config.APIKey
-	phones = config.Phones
-	smsPrefix = config.SmsPrefix
+	YP = config.YPInfo
+	mailInfo = config.MailInfo
 
 	for _, item := range config.TestItem {
 		go itemTest(item)
@@ -64,17 +78,19 @@ func itemTest(item testItem) {
 					lastResult[name] = true
 				}
 				if !ok {
+					fmt.Printf("[%s]: %s\n", name, err)
 					if lastResult[name] == true {
 						sendMSG(fmt.Sprintf("[%s](%s) 请求接口失败: %s", name, ip, err))
+						sendEmail(fmt.Sprintf("[%s](%s) 请求接口失败: %s", name, ip, err))
 						lastResult[name] = false
 					}
-					fmt.Printf("[%s]: %s\n", name, err)
 				} else {
+					fmt.Printf("[%s]: OK\n", name)
 					if lastResult[name] == false {
 						sendMSG(fmt.Sprintf("[%s](%s) 恢复正常", name, ip))
+						sendEmail(fmt.Sprintf("[%s](%s) 恢复正常", name, ip))
 						lastResult[name] = true
 					}
-					fmt.Printf("[%s]: OK\n", name)
 				}
 				sig <- 1
 			}(ip, name)
@@ -109,8 +125,8 @@ func testServer(serverIP, proto, url, requestTimeout string) (bool, error) {
 	if err != nil {
 		return false, err
 	}
-	if resp.StatusCode != http.StatusOK {
-		return false, errors.New("status code not ok")
+	if resp.StatusCode >= 500 {
+		return false, errors.New(fmt.Sprintf("status code(%d) not ok", resp.StatusCode))
 	}
 
 	// var f interface{}
@@ -127,12 +143,34 @@ func testServer(serverIP, proto, url, requestTimeout string) (bool, error) {
 	return true, nil
 }
 
+func sendEmail(msg string) {
+	if !mailInfo.Enable {
+		return
+	}
+	auth := smtp.PlainAuth("", mailInfo.Username, mailInfo.Password, mailInfo.Smtp)
+	to := mailInfo.To
+	nickname := "服务器小管家"
+	user := mailInfo.Username
+	subject := "服务器异常告警"
+	content_type := "Content-Type: text/plain; charset=UTF-8"
+	body := msg
+	context := []byte("To: " + strings.Join(to, ",") + "\r\nFrom: " + nickname +
+		"<" + user + ">\r\nSubject: " + subject + "\r\n" + content_type + "\r\n\r\n" + body)
+	err := smtp.SendMail("smtp.qq.com:25", auth, user, to, context)
+	if err != nil {
+		fmt.Printf("send mail error: %v", err)
+	}
+}
+
 func sendMSG(msg string) {
+	if !YP.Enable {
+		return
+	}
 	// 发送短信
-	clnt := ypclnt.New(APIKey)
+	clnt := ypclnt.New(YP.APIKey)
 	param := ypclnt.NewParam(2)
-	param[ypclnt.TEXT] = smsPrefix + msg
-	for _, phone := range phones {
+	param[ypclnt.TEXT] = YP.SmsPrefix + msg
+	for _, phone := range YP.Phones {
 		param[ypclnt.MOBILE] = phone
 		clnt.Sms().SingleSend(param)
 	}
